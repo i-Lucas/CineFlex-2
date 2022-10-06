b94b155edcc3e9d424c573df97bad3f524dacf27
i-Lucas <novo.contato.lucas@gmail.com>
2022-10-05 23:34:16 -0300

    feat change app font

--- src/components/fonts/index.js
+++ src/components/fonts/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Roboto = styled.h1`
 
-    font-family: 'Roboto';
+    font-family: 'Recursive';
     color: ${props => props.color};
     font-size: ${props => props.size};
 `;
