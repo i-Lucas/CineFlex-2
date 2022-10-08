776e0d587f60ee09f63d7e04735eecb6eda9c0ce
i-Lucas <novo.contato.lucas@gmail.com>
2022-10-08 04:03:29 -0300

    fix remove back button from main page

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 export default function Header({ page }) {
 
     const navigate = useNavigate();
-    const sucess = window.location.pathname === "/sucess";
+    const sucess = window.location.pathname === "/sucess" || window.location.pathname === "/"
 
     function RenderButton() {
 
